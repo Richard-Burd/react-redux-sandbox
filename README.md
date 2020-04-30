4c71c88926177885f741dbe72844f307fe07cb27
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-04-30 11:59:40 -0700

    Lesson 26 finished

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ https://www.youtube.com/watch?v=9boMnm5X9ak
 ## React Only
 https://www.youtube.com/playlist?list=PLC3y8-rFHvwgg3vaYJgHGnModB54rxOk3
 
-## Left off here with Vishwas
-https://www.youtube.com/watch?v=4ORZ1GmjaMc&list=PLC3y8-rFHvwgg3vaYJgHGnModB54rxOk3&index=25
+## Left off Lesson 26 with Vishwas
+https://www.youtube.com/watch?v=4ORZ1GmjaMc&list=PLC3y8-rFHvwgg3vaYJgHGnModB54rxOk3&index=26
--- react-codealong/src/components/Columns.js
+++ react-codealong/src/components/Columns.js
@@ -5,8 +5,21 @@
 import React from 'react'
 
 function Columns() {  // made with the "rfce" snippet
+
+  // React.Fragment can accept key attributes when rendering lists of items...
+  // ...and you can use the "key" attribute to render them as shown in line 17 below
+  const myItems = [{title: "1st Title", id: "1"}, {title: "2nd Title", id: "2"}]
+
   return (
     <React.Fragment>
+      {
+        myItems.map(item => (
+          <React.Fragment key={item.id}>
+            <h1>Title</h1>
+            <p>{item.title}</p>
+          </React.Fragment>
+        ))
+      }
       <td>Name</td>
       <td>Vishwas</td>
     </React.Fragment>
@@ -14,3 +27,18 @@ function Columns() {  // made with the "rfce" snippet
 }
 
 export default Columns
+
+// NOTE: instead of using "React.fragment", you could just use a shorthand syntax
+// wherein you use empty opening & closing tags like so:
+
+/*
+
+return (
+  <>
+   <p>S.thing in here</p>
+  </>
+)
+
+WARNING: if you use this syntax, you cannot use the "key" attribute as shown above in line 17
+
+*/
