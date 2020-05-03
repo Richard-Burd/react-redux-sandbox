6404c72e5d00208e01b2ce27bb0729f2ec838f2e
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-05-03 15:24:05 -0700

    Render Props App File Created

--- react-codealong/src/App.js (Render Props).js
+++ react-codealong/src/App.js (Render Props).js
@@ -1,3 +1,7 @@
+// Lesson 37 (https://www.youtube.com/watch?v=EZil2OTyB4w&list=PLC3y8-rFHvwgg3vaYJgHGnModB54rxOk3&index=37)
+// This concearns Render Props which refers to a technique for sharing code between
+// React components using a prop whose value is a function.
+
 import React, { Component } from 'react';
 import './App.css';
 import ClickCounterTwo from './components/ClickCounterTwo'
